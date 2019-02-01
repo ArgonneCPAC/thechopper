5c2a8881dd69b8a3eac76a4edc447f721cd3d4ce
Andrew Hearin <ahearin@anl.gov>
2019-02-01 16:15:27 -0600

    Added numpy to install_requires

--- setup.py
+++ setup.py
@@ -12,5 +12,6 @@ setup(
     author="Andrew Hearin",
     author_email="ahearin@anl.gov",
     description="Python tools to subdivide cosmological simulations and tabulate synthetic observables",
-    long_description="Python tools to subdivide cosmological simulations and tabulate synthetic observables"
+    long_description="Python tools to subdivide cosmological simulations and tabulate synthetic observables",
+    install_requires=["numpy"]
 )
