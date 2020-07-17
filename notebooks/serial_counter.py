62e9473ad85a11002bae329f903c990d8e264f71
Antonio Villarreal <avillarreal2anl.gov>
2020-07-17 15:28:13 -0400

    removed extraneous comparison loop in serial counter

--- notebooks/serial_counter.py
+++ notebooks/serial_counter.py
@@ -34,19 +34,10 @@ for fname0, fname1 in zip(sample0_fnames, sample1_fnames):
     all_x1.extend(x1)
     all_y1.extend(y1)
     all_z1.extend(z1)
-    print('number of points: {}'.format(len(x0)))
-    pos0 = np.vstack((x0, y0, z0)).T
-    pos1 = np.vstack((x1, y1, z1)).T
-    tree0 = cKDTree(pos0, boxsize=1000)
-    tree1 = cKDTree(pos1, boxsize=1000)
-    these_counts = tree0.count_neighbors(tree1, rmax)
-    print("For rank {0} filenum {1}, counts = {2}".format('0',
-        os.path.basename(fname0).split('.')[0].split('_')[-1], these_counts))
-    counts += these_counts
-print("Total counts = {0}".format(counts))
 allpos0 = np.vstack((all_x0, all_y0, all_z0)).T
 allpos1 = np.vstack((all_x1, all_y1, all_z1)).T
 alltree0 = cKDTree(allpos0, boxsize=1000)
 alltree1 = cKDTree(allpos1, boxsize=1000)
 all_counts = alltree0.count_neighbors(alltree1, rmax)
-print("Total counts (alt) = {}".format(all_counts))
+print("Total counts = {}".format(all_counts))
+
