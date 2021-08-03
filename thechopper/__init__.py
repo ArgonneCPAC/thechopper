c61a528b67e9c75f97f432645fc0f4018d2675a3
Antonio Villarreal <avillarreal2anl.gov>
2021-08-03 16:19:54 -0400

    preserves changes from deltasigma set-up

--- thechopper/__init__.py
+++ thechopper/__init__.py
@@ -1,4 +1,4 @@
 """For further details about thechopper, see https://www.youtube.com/watch?v=-9-Te-DPbSE
 """
 from .buffered_subvolume_calculations import *
-from .data_chopper import get_chopped_data, assign_chopped_data_to_ranks
+from .data_chopper import get_chopped_data, assign_chopped_data_to_ranks, get_data_for_rank
